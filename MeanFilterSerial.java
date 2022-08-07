cb4381518cfcb17caedbefc026cb98a7eee5cc2d
maqhobosheane <maqhobosheane@outlook.com>
2022-08-07 23:15:04 +0200

    First finished serial mean filter. Bugs: Averages sums going forward and downwards which goes out of bounds towards the end of the image width/height.

--- MeanFilterSerial.java
+++ MeanFilterSerial.java
@@ -21,13 +21,33 @@ public class MeanFilterSerial{
     
     int width = input.getWidth();
     int height = input.getHeight();
+    int sum = 0;
+    int average;
+    int winSize = Integer.valueOf(args[2]);
     ArrayList<Integer> pixels = new ArrayList<Integer>();
     
     for(int count = 0; count < width; count++){
-      for (int counter = 0; counter < height; counter++){
-        int p = input.getRGB(count,counter);
-        pixels.add(p); 
+      for (int counter = 0; counter < height ; counter++){
+        for(int winCount = 0; winCount < winSize;winCount++){
+           for(int winCounter = 0; winCounter < winSize;winCounter++){       
+             sum = sum + input.getRGB(count+winCount,counter+winCounter);
+          }
+          
+        }
+         
+        average = sum/(winSize*winSize);
+        input.setRGB(count,counter,average);  
       }
+
+    }
+    
+     //write image
+    try{
+      f = new File("/home/maqhobosheane/Downloads/Output.jpeg");
+      ImageIO.write(input, "jpg", f);
+      System.out.println("Image successfully written to.");
+    }catch(IOException e){
+      System.out.println(e);
     }
     
     }

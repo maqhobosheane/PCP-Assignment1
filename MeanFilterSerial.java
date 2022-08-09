5ca4d74d5d6e41e480441d055a0fdc47e385d6e7
maqhobosheane <maqhobosheane@outlook.com>
2022-08-09 03:00:48 +0200

    All pixels averaged by colour, output is definitely incorrect.

--- MeanFilterSerial.java
+++ MeanFilterSerial.java
@@ -12,7 +12,7 @@ public class MeanFilterSerial{
 
     //read image
     try{
-      f = new File("/home/maqhobosheane/Downloads/Image.jpeg");
+      f = new File("/home/maqhobosheane/Downloads/Image.jpg");
       input = ImageIO.read(f);
       System.out.println("Image successfully read.");
     }catch(IOException e){
@@ -21,8 +21,20 @@ public class MeanFilterSerial{
     
     int width = input.getWidth();
     int height = input.getHeight();
-    int sum = 0;
-    int average;
+    int p;
+    int a;
+    int r;
+    int g;
+    int b;
+    int sumA = 0;
+    int sumR = 0;
+    int sumG = 0;
+    int sumB = 0;
+    int avgA;
+    int avgR;
+    int avgG;
+    int avgB;
+    int avgPixel;
     int winSize = Integer.valueOf(args[2]);
     ArrayList<Integer> pixels = new ArrayList<Integer>();
     
@@ -31,27 +43,63 @@ public class MeanFilterSerial{
       for (int counter = 0; counter < height ; counter++){
         for(int winCount = 0; winCount < winSize;winCount++){
            for(int winCounter = 0; winCounter < winSize;winCounter++){
-           if(count <= width - winSize && counter <= height - winSize ){       
-             sum = sum + input.getRGB(count+winCount,counter+winCounter);
+           if(count <= width - winSize && counter <= height - winSize ){ 
+             p = input.getRGB(count + winCount,counter + winCounter);
+             a = (p>>24) & 0xff;
+             r = (p>>16) & 0xff;
+             g = (p>>8) & 0xff;   
+             b = p & 0xff;   
+             sumA = sumA + a;
+             sumR = sumR + r;
+             sumG = sumG + g;
+             sumB = sumB + b;
            }
            else if(count > width - winSize && counter > height - winSize){
-             sum = sum + input.getRGB(count-winCount,counter-winCounter);
+             p = input.getRGB(count - winCount,counter - winCounter);
+             a = (p>>24) & 0xff;
+             r = (p>>16) & 0xff;
+             g = (p>>8) & 0xff;   
+             b = p & 0xff;   
+             sumA = sumA + a;
+             sumR = sumR + r;
+             sumG = sumG + g;
+             sumB = sumB + b;
            }
            else if(count > width - winSize && counter <= height - winSize){
            
-             sum = sum + input.getRGB(count-winCount,counter+winCounter);
+             p = input.getRGB(count - winCount,counter + winCounter);
+             a = (p>>24) & 0xff;
+             r = (p>>16) & 0xff;
+             g = (p>>8) & 0xff;   
+             b = p & 0xff;   
+             sumA = sumA + a;
+             sumR = sumR + r;
+             sumG = sumG + g;
+             sumB = sumB + b;
              
            }
            else if(count <= width - winSize && counter > height - winSize){
-             sum = sum + input.getRGB(count+winCount,counter-winCounter);
+             p = input.getRGB(count + winCount,counter - winCounter);
+             a = (p>>24) & 0xff;
+             r = (p>>16) & 0xff;
+             g = (p>>8) & 0xff;   
+             b = p & 0xff;   
+             sumA = sumA + a;
+             sumR = sumR + r;
+             sumG = sumG + g;
+             sumB = sumB + b;
            
            }
           }
           
         }
          
-        average = sum/(winSize*winSize);
-        input.setRGB(count,counter,average);  
+        avgA = sumA/(winSize*winSize);
+        avgR = sumA/(winSize*winSize);
+        avgG = sumA/(winSize*winSize);
+        avgB = sumA/(winSize*winSize);
+        avgPixel = (avgA<<24) | (avgR<<16) | (avgG<<8) | avgB;
+        input.setRGB(count,counter,avgPixel);  
       }
 
     }
@@ -60,7 +108,7 @@ public class MeanFilterSerial{
    
      //write image
     try{
-      f = new File("/home/maqhobosheane/Downloads/Output.jpeg");
+      f = new File("/home/maqhobosheane/Downloads/Output.jpg");
       ImageIO.write(input, "jpg", f);
       System.out.println("Image successfully written to.");
     }catch(IOException e){
